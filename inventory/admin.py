c52ac555b4c67ae53f79b0961ba52d6ef9e384e0
Jens Timmerman <jens.timmerman@gmail.com>
2018-09-16 13:14:54 +0200

    added mvp

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PantryItem, PantryItemLine
+
+class PantryItemInLine(admin.TabularInline):
+    model = PantryItemLine
+    extra = 1
+
+class PantryItemInLineAdmin(admin.ModelAdmin):
+    list_filter = ['expiry_date']
+
+class PantryItemAdmin(admin.ModelAdmin):
+    inlines = [PantryItemInLine]
+
+admin.site.register(PantryItem, PantryItemAdmin)
+admin.site.register(PantryItemLine, PantryItemInLineAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+CATEGORIES = (
+    ('UC', 'UNCATEGORIZED'),  # bloem meel pasta
+    ('GS', 'GENRAL_STOCK'),  # bloem meel pasta
+    ('CANS', 'CANS'),
+    ('BR', 'BREAKFAST'),
+    ('SAV', 'SAVORIES'),
+    ('SW', 'SWEETS'),
+    ('SN', 'SNACKS'),
+    ('SP', 'SPICES'),
+)
+
+
+class PantryItem(models.Model):
+    name =  models.CharField(max_length=200)
+    category = models.CharField(max_length=200, choices=CATEGORIES)
+    min_quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class PantryItemLine(models.Model):
+    pantry_item = models.ForeignKey(PantryItem, on_delete=models.PROTECT, default='UN' )
+    quantity = models.IntegerField(default=1)
+    expiry_date = models.DateField()
+
+    def __str__(self):
+        return self.pantry_item.name + ' ' + self.quantity
