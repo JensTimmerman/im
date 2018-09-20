26980d4ad7692f26d9840757901e49af9a2dd870
Jens Timmerman <jens.timmerman@gmail.com>
2018-09-20 23:00:24 +0200

    Added some things to publish this app

--- README.md
+++ README.md
@@ -3,3 +3,39 @@ Pantry inventory management
 
 # Inventory management system written in django - python
 The object of this django app is to keep track of which goods you own, when they will expire, when you should consume them and when you shoud buy more.
+
+# Getting started
+`pip3 install django`
+
+
+1. Add "inventory" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'inventory',
+    ]
+
+2. Include the inventory URLconf in your project urls.py like this::
+
+    path('inventory/', include('inventory.urls')),
+
+3. Run `python manage.py migrate` to create the polls models.
+
+4. Start the development server and visit http://127.0.0.1:8000/admin/
+   to create a pantry inventory (you'll need the Admin app enabled).
+
+5. Visit http://127.0.0.1:8000/inventory/ to view your inventory website.
+
+# feature requests
+## High
+- shopping list based on min quantity
+
+- one off shoping list
+
+- easy clearing of shopping list
+- correct capilisation
+
+## low
+- add recepies
+- shopping list created based on recepy ingredients
+- auto proposal of recepy based on next expiry dates
---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-inventory',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    license='AGPL v3',
+    description='A simple Django app to keep your pantry inventory.',
+    long_description=README,
+    author='Jens Timmerman',
+    author_email='jens.timmerman@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.1',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: AGPL',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
